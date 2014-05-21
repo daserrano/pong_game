95b4697ce128683b6635aa770202398851c8e93d
David <david@david-VirtualBox.(none)>
2014-05-21 18:34:05 +0200

    problems with moves of player2

--- pong.cpp
+++ pong.cpp
@@ -86,16 +86,16 @@ void RellenarJugador1(Coordenada jugador1[MAX]){
 void RellenarJugador2(Coordenada jugador2[MAX]){
 
     for(int i=0; i < MAX ; i++){
-	jugador2[i].x = 30;
+	jugador2[i].x = 93;
 	jugador2[i].y = 15 + i;
     }
 
 }
 
 void MuestraJugador(Coordenada jugador1[MAX]){
-    clear();
+    /*clear();
     PintadoTablero();
-
+*/
     attron(COLOR_PAIR(2));
     for(int i=0; i < MAX; i++)
 	
@@ -106,14 +106,14 @@ void MuestraJugador(Coordenada jugador1[MAX]){
 
 }
 
-void MuestraJugador2(Coordenada jugador1[MAX]){
-    clear();
+void MuestraJugador2(Coordenada jugador2[MAX]){
+   /* clear();
     PintadoTablero();
-
+*/
     attron(COLOR_PAIR(2));
     for(int i=0; i < MAX; i++)
 	
-	mvprintw(jugador1[i].y, jugador1[i].x, "||");
+	mvprintw(jugador2[i].y, jugador2[i].x, "||");
 
     attroff(COLOR_PAIR(2));
     refresh();
@@ -138,12 +138,31 @@ void MoverJugadorAbajo(Coordenada jugador[MAX], Coordenada incremento){
 
 }  
 
+void MoverJugador2Arriba(Coordenada jugador[MAX], Coordenada incremento){
+
+    for(int i= MAX-1; i>0; i--)
+	jugador[i].y = jugador[i-1].y;
+
+    jugador[0].y += incremento.y;
+
+}
+
+void MoverJugador2Abajo(Coordenada jugador[MAX], Coordenada incremento){
+
+    for(int i=0; i<MAX; i++)
+	jugador[i-1].y = jugador[i].y;
+
+    jugador[MAX-1].y += incremento.y;
+
+} 
+
 int main(int argc, char *argv[]){
 
     int user_input = 0;
     Coordenada jugador1[MAX];
     Coordenada jugador2[MAX];
-    Coordenada movimiento = {0 , 1};
+    Coordenada movimiento1 = {0 , 1};
+    Coordenada movimiento2 = {0 , -1};
 
     initscr(); // iniciar tablero de ncurses.
     start_color();
@@ -151,7 +170,8 @@ int main(int argc, char *argv[]){
     init_pair(1, COLOR_BLUE, COLOR_BLUE); // Colorear.
     init_pair(2, COLOR_WHITE, COLOR_WHITE);
 
-    //halfdelay(2);
+    // ------------------------------------NO FUNCIONA EL HALFDELAY-------------------------------------
+    halfdelay(2);
     keypad(stdscr, TRUE); // Poder utilizar las flechas.
     noecho(); //No se muestre el caracter pulsado.
     curs_set(0); // Desaparecer el puntero.
@@ -166,36 +186,39 @@ int main(int argc, char *argv[]){
     clear();   // Limpia la pantalla.
 
     do{
+
+    halfdelay(2);
 	switch(tolower(user_input = getch())){    // Es lo mismo mayuscula que minuscula.
 	    case 'w':
-		movimiento.x = 0;
-		movimiento.y = -1;
-		MoverJugadorArriba(jugador1, movimiento);
+		movimiento1.x = 0;
+		movimiento1.y = -1;
+		MoverJugadorArriba(jugador1, movimiento1);
 		break;
 
 	    case 's':
-		movimiento.x = 0;
-		movimiento.y = 1;
-		MoverJugadorAbajo(jugador1, movimiento);
+		movimiento1.x = 0;
+		movimiento1.y = 1;
+		MoverJugadorAbajo(jugador1, movimiento1);
 		break;
 
 	    case KEY_UP:
-		movimiento.x = 0;
-		movimiento.y = -1;
-		//MoverJugadorArriba(jugador2, movimiento);
+		movimiento2.x = 0;
+		movimiento2.y = -1;
+		MoverJugador2Arriba(jugador2, movimiento2);
 		break;
 
-	    case KEY_DOWN:
-		movimiento.x = 0;
-		movimiento.y = 1;
-		//MoverJugadorAbajo(jugador2, movimiento);
+	    case KEY_DOWN:  // ----------------------------- FUNCIONA MAL, SE BAJA TAMBIEN EL USUARIO 1. ----------------------
+		movimiento2.x = 0;
+		movimiento2.y = 1;
+		MoverJugador2Abajo(jugador2, movimiento2);
 		break;
 
 	}
 
-
+	clear();
+	PintadoTablero();
 	MuestraJugador(jugador1);
-        //MuestraJugador2(jugador2);
+	MuestraJugador2(jugador2);
 
     }while(user_input = getch() != ESC);
     endwin(); // Finaliza el tablero de ncurses.
