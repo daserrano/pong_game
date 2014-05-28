0864c4157d4cd0062dd01b4931f4993997c2350b
David Serrano Indiano <dserrano@dam1-pc00.(none)>
2014-05-28 10:43:09 +0200

    update with moves and area

--- pong.cpp
+++ pong.cpp
@@ -15,6 +15,13 @@ typedef struct TCoordenada{
 
 } Coordenada; 
 
+typedef struct TPelota{
+
+    Coordenada posicion;
+    Coordenada velocidad;
+
+} Pelota;   
+
 void PintadoPresentacion(){
 
     system("clear");
@@ -30,7 +37,7 @@ void PintadoPresentacion(){
 
     char pixel;
     while( (pixel = fgetc(fp)) != EOF){
-	    printw("%c", pixel);
+	printw("%c", pixel);
     }
 
 
@@ -39,9 +46,8 @@ void PintadoPresentacion(){
 
 } 
 
-void PintadoTablero(){
+void PintadoTablero(Pelota pelota){
 
-    system("clear");
 
     char tablero[MAXLINES];
     FILE *fp;
@@ -67,11 +73,12 @@ void PintadoTablero(){
 	    printw("%c", pixel);
 
     }
-
-    //attroff(COLOR_PAIR(1));
+    attron(COLOR_PAIR(3));
+    mvprintw(pelota.posicion.y, pelota.posicion.x, "O");
+    attroff(COLOR_PAIR(3));
     refresh();
     fclose(fp);
-}
+} 
 
 void RellenarJugador1(Coordenada jugador1[MAX]){
 
@@ -90,14 +97,14 @@ void RellenarJugador2(Coordenada jugador2[MAX]){
     }
 
 }
-
+ 
 void MuestraJugador(Coordenada jugador1[MAX]){
     /*clear();
-    PintadoTablero();
-*/
+      PintadoTablero();
+     */
     attron(COLOR_PAIR(2));
     for(int i=0; i < MAX; i++)
-	
+
 	mvprintw(jugador1[i].y, jugador1[i].x, "||");
 
     attroff(COLOR_PAIR(2));
@@ -106,12 +113,12 @@ void MuestraJugador(Coordenada jugador1[MAX]){
 }
 
 void MuestraJugador2(Coordenada jugador2[MAX]){
-   /* clear();
-    PintadoTablero();
-*/
+    /* clear();
+       PintadoTablero();
+     */
     attron(COLOR_PAIR(2));
     for(int i=0; i < MAX; i++)
-	
+
 	mvprintw(jugador2[i].y, jugador2[i].x, "||");
 
     attroff(COLOR_PAIR(2));
@@ -130,18 +137,34 @@ void MoverJugadorArriba(Coordenada jugador[MAX], Coordenada incremento){
 
 void MoverJugadorAbajo(Coordenada jugador[MAX], Coordenada incremento){
 
-    for(int i=0; i<MAX; i++)
-	jugador[i-1].y = jugador[i].y;
+    for(int i=0; i<MAX-1; i++)
+	jugador[i].y = jugador[i+1].y;
 
     jugador[MAX-1].y += incremento.y;
 
 }  
+
+void MovimientoPelota( Coordenada jugador1[MAX], Coordenada jugador2[MAX], Pelota *pelota){
+
+    for(int i=0; i < MAX-1; i++){
+    if(pelota->posicion.y == jugador2[0].y && pelota->velocidad.x > 0 && (pelota->posicion.x)+1 == jugador2[0].x ){
+	pelota->velocidad.x = -1;
+	pelota->velocidad.y = -2;
+	break;
+    }
+    if(pelota->posicion.y == jugador2[i].y && pelota->velocidad.x > 0 && (pelota->posicion.x)+1 == jugador2[i].x)
+	pelota->velocidad.x = 1;
+    if(pelota->posicion.y == jugador1[i].y && pelota->velocidad.x < 0 && (pelota->posicion.x)-1 == jugador1[i].x)
+	pelota->velocidad.x = 1;
+    }
+}
 
 int main(int argc, char *argv[]){
 
     int user_input = 0;
     Coordenada jugador1[MAX];
     Coordenada jugador2[MAX];
+    Pelota pelota = {10, 10};
     Coordenada movimiento1 = {0 , 1};
     Coordenada movimiento2 = {0 , 1};
 
@@ -150,9 +173,10 @@ int main(int argc, char *argv[]){
 
     init_pair(1, COLOR_BLUE, COLOR_BLUE); // Colorear.
     init_pair(2, COLOR_WHITE, COLOR_WHITE);
+    init_pair(3, COLOR_WHITE, COLOR_BLACK);
 
     // ------------------------------------NO FUNCIONA EL HALFDELAY-------------------------------------
-    halfdelay(2);
+    //halfdelay(2);
     keypad(stdscr, TRUE); // Poder utilizar las flechas.
     noecho(); //No se muestre el caracter pulsado.
     curs_set(0); // Desaparecer el puntero.
@@ -166,43 +190,55 @@ int main(int argc, char *argv[]){
     }
     clear();   // Limpia la pantalla.
 
+    pelota.velocidad.x = 1;
+    pelota.velocidad.y = 0;
+    //timeout ( 10 );
     do{
-
-    halfdelay(2);
-	switch(tolower(user_input = getch())){    // Es lo mismo mayuscula que minuscula.
+	timeout(50);
+	user_input = getch();
+	switch(user_input){    // Es lo mismo mayuscula que minuscula.
 	    case 'w':
 		movimiento1.x = 0;
 		movimiento1.y = -1;
-		MoverJugadorArriba(jugador1, movimiento1);
+		if(jugador1[0].y > 2)
+		    MoverJugadorArriba(jugador1, movimiento1);
 		break;
 
 	    case 's':
 		movimiento1.x = 0;
 		movimiento1.y = 1;
-		MoverJugadorAbajo(jugador1, movimiento1);
+		if(jugador1[MAX-1].y < 32)
+		    MoverJugadorAbajo(jugador1, movimiento1);
 		break;
 
 	    case KEY_UP:
 		movimiento2.x = 0;
 		movimiento2.y = -1;
-		MoverJugadorArriba(jugador2, movimiento2);
+		if(jugador2[0].y > 2)
+		    MoverJugadorArriba(jugador2, movimiento2);
 		break;
 
 	    case KEY_DOWN:  // ----------------------------- FUNCIONA MAL, SE BAJA TAMBIEN EL USUARIO 1. ----------------------
 		movimiento2.x = 0;
 		movimiento2.y = 1;
-		MoverJugadorAbajo(jugador2, movimiento2);
+		if(jugador2[MAX-1].y < 32)
+		    MoverJugadorAbajo(jugador2, movimiento2);
 		break;
 
 
 	}
 
-	clear();
-	PintadoTablero();
+
+
+	MovimientoPelota(jugador1, jugador2, &pelota);
+	pelota.posicion.x += pelota.velocidad.x; 
+	pelota.posicion.y += pelota.velocidad.y;
+	erase();
+	PintadoTablero(pelota);
 	MuestraJugador(jugador1);
 	MuestraJugador(jugador2);
 
-    }while(user_input = getch() != ESC);
+    }while(user_input != ESC);
     endwin(); // Finaliza el tablero de ncurses.
 
     return EXIT_SUCCESS;
